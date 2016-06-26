d9a40a48c03db9a1e272090cdf55de1a6660e633
gintro <gintronati@gmail.com>
2016-06-26 09:22:55 +0200

    code analyze deleted

--- main.py
+++ main.py
@@ -26,6 +26,9 @@ currentDate = datetime(now.year,now.month,now.day,0,0)
 ## Open file to write errors
 f_errors = open('logs', "w")
 
+# Table uses an index:
+# db.valuesH.createIndex({name:1 , date:1},{unique:true})
+
 for elem in listStockIds:
 	try:
 		tree = etree.parse('http://www.puentenet.com/cotizaciones/accionesCotizaciones!getAccionPorId?id=ACCION_'+elem,parser)
@@ -39,23 +42,8 @@ for elem in listStockIds:
 		f_errors.write("Error ("+str(e)+" ) occured for "+elem+"\n----\n")
 	
 # The resulting dataSet is a dict with keys X:training dataSet and y:targetValues
-# Table uses an index:
-# db.valuesH.createIndex({name:1 , date:1},{unique:true})
-
-#dataSet = data.buildDataSet(myDict)
 
 f_errors.close()
 
 ## Analyse stage
-#
-# Normalize the data and store the params 
-'''std_scale = preprocessing.StandardScaler().fit(dataSet['X'])
-X_train_std = std_scale.transform(dataSet['X'])	
 
-#fit the model
-regressionModel = fitting.fitModel(( X_train_std , dataSet['y']) ) #return value is a model object
-X_test_std = std_scale.transform(dataSet['X'][3,:])
-
-print regressionModel.coef_ 
-print myDict['APBR']['currentPrice'], " and the predicted value is ", regressionModel.predict(X_test_std) 
-'''
